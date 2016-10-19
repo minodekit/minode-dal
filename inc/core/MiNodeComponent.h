884d4e4a8e68f6c30bcb6ea9ac6aa26a457efe2c
Yongjia Niu <ahnniu@gmail.com>
2016-10-19 15:57:30 +0800

    Modify the enums format, since we want use these in microsoft touch develop

--- inc/core/MiNodeComponent.h
+++ inc/core/MiNodeComponent.h
@@ -8,34 +8,41 @@
 #define MINODE_ID_START                 100
 #define MINODE_ID_NC                    199
 
-#define MINODE_ID_A0                    MINODE_ID_START
-#define MINODE_ID_A1                    MINODE_ID_A0 + 2
-#define MINODE_ID_A2                    MINODE_ID_A0 + 4
+#define MINODE_ID_A0                    100
+#define MINODE_ID_A1                    102
+#define MINODE_ID_A2                    104
 
-#define MINODE_ID_D_START               MINODE_ID_START + 10
-#define MINODE_ID_D0                    MINODE_ID_NC
-#define MINODE_ID_D1                    MINODE_ID_NC
-#define MINODE_ID_D2                    MINODE_ID_NC
-#define MINODE_ID_D3                    MINODE_ID_NC
-#define MINODE_ID_D4                    MINODE_ID_NC
-#define MINODE_ID_D5                    MINODE_ID_NC
-#define MINODE_ID_D6                    MINODE_ID_NC
-#define MINODE_ID_D7                    MINODE_ID_NC
-#define MINODE_ID_D8                    MINODE_ID_NC
-#define MINODE_ID_D9                    MINODE_ID_NC
-#define MINODE_ID_D10                   MINODE_ID_NC
-#define MINODE_ID_D11                   MINODE_ID_NC
-#define MINODE_ID_D12                   MINODE_ID_D_START + 24
-#define MINODE_ID_D13                   MINODE_ID_D_START + 26
-#define MINODE_ID_D14                   MINODE_ID_D_START + 28
-#define MINODE_ID_D15                   MINODE_ID_D_START + 30
-
-
+#define MINODE_ID_D_START               110
+#define MINODE_ID_D0                    199
+#define MINODE_ID_D1                    199
+#define MINODE_ID_D2                    199
+#define MINODE_ID_D3                    199
+#define MINODE_ID_D4                    199
+#define MINODE_ID_D5                    199
+#define MINODE_ID_D6                    199
+#define MINODE_ID_D7                    199
+#define MINODE_ID_D8                    199
+#define MINODE_ID_D9                    199
+#define MINODE_ID_D10                   199
+#define MINODE_ID_D11                   199
+#define MINODE_ID_D12                   134
+#define MINODE_ID_D13                   136
+#define MINODE_ID_D14                   138
+#define MINODE_ID_D15                   140
 
 
-#define MINODE_ID_MODULE_START          120
-#define MINODE_ID_MODULE_SWITCH         MINODE_ID_MODULE_START
 
+
+#define MINODE_ID_MODULE_START          150
+#define MINODE_ID_MODULE_SWITCH         150
+
+typedef enum {
+  SWITCH_A = 151,
+  SWITCH_B = 152,
+  SWITCH_C = 153,
+  SWITCH_D = 154,
+  SWITCH_E = 155
+}SwitchName;
 
 
 
